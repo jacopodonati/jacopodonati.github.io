9f83177eabe0df5d1ed7eb937050ee478d5c2488
Jacopo Donati <33698919+jacopodonati@users.noreply.github.com>
2022-09-04 15:18:12 +0200

    Aggiornamento dieta

--- i-ching/53.js
+++ i-ching/53.js
@@ -1,7 +1,7 @@
 export const hex53 = {
     last_update: '2022-07-29T12:58:39+02:00',
     symbol: '䷴',
-    name: 'La gradualità',
+    name: 'Lo sviluppo graduale',
     judgement: '',
     image: '',
     lines: [
