1799db9059dd81092d64b96324a13b57c949d595
Jacopo Donati <33698919+jacopodonati@users.noreply.github.com>
2023-03-26 14:51:05 +0200

    IC: Aggiunta esagramma 4

--- assets/js/i-ching/4.js
+++ assets/js/i-ching/4.js
@@ -1,16 +1,16 @@
 export const hex4 = {
-    last_update: '2022-07-29T12:58:39+02:00',
+    last_update: '2023-03-26T14:35:00+02:00',
     symbol: '䷃',
     number: 4,
     name: 'L’ignoranza',
-    judgement: '',
-    image: '',
+    judgement: 'L’ignoranza giovanile ha riuscita. \n Io non cerco il giovane ignorante, lui cerca me. \n La prima volta l’oracolo risponde. Se chiede ancora l‘oracolo confonde e, di conseguenza, non dà risposta. \n Propizia è perseveranza.',
+    image: 'Da dentro la montagna sgorga una fonte: l’ignoranza. \n Così il discepolo si muove con cura alimentando la virtù.',
     lines: [
-        '',
-        '',
-        '',
-        '',
-        '',
-        '',
+        'Per sviluppare l’ignorante, è propizio disciplinare l’uomo. \n Bisogna togliere vincoli. Continuare così reca svergognamento.',
+        'Sopportare l’ignorante porta salute. \n Saper prendere le donne porta salute. \n Il figlio controlla la casa.',
+        'Non prendere la ragazza che vede un uomo di bronzo e non rimane padrona di sé stessa. \n\ Nulla è propizio.',
+        'L’ignoranza porta a svergognamento.',
+        'L’ignoranza giovanile porta salute.',
+        'Quando si punisce l’ignoranza, non bisogna commettere soprusi.',
     ]
 }
