4ae232acfc23d8b84f98a911070b652071fb04ef
Jacopo Donati <33698919+jacopodonati@users.noreply.github.com>
2023-03-27 13:30:13 +0200

    IC: linee dell'esagramma 5

--- assets/js/i-ching/5.js
+++ assets/js/i-ching/5.js
@@ -1,16 +1,16 @@
 export const hex5 = {
-    last_update: '2022-07-29T12:58:39+02:00',
+    last_update: '2023-03-27T13:29:56+02:00',
     symbol: '䷄',
     number: 5,
     name: 'L’attesa',
-    judgement: '',
-    image: '',
+    judgement: 'L’attesa: se sei sincero, hai luce e riuscita. \n Perseveranza reca salute. \n Propizio è lasciarsi condurre dagli eventi.',
+    image: 'Le nuvole salgono oltre il cielo: l’ attesa. \n Così il discepolo mangia e beve e riposa con piacere.',
     lines: [
-        '',
-        '',
-        '',
-        '',
-        '',
-        '',
+        'Attendere nella periferia: propizio è rimanere perseveranti. \n Nessuna macchia.',
+        'Attendere nella sabbia: qualche battibecco ma la fine reca salute.',
+        'Attendere nel fango provoca la venuta del nemico.',
+        'Attendere nel sangue. Fuori da questa buca!',
+        'Attendere bevendo e mangiando: perseveranza reca salute.',
+        'Entrare nella buca. \n Senza fretta, arrivano tre ospiti: onorali e arriverà la salute.',
     ]
 }
