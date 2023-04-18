e2292119d98c7ff1d586616ec102247bf03f766c
Jacopo Donati <33698919+jacopodonati@users.noreply.github.com>
2023-04-18 12:10:49 +0200

    Semplificazione location

--- assets/js/whereami.js
+++ assets/js/whereami.js
@@ -1,44 +1,48 @@
 ---
 ---
+{%- assign all_locations = site.data.locations %}
+{%- assign locations = '' | split: '' %}
+{%- assign today = 'now' | date: '%s' %}
+{%- assign index = 0 %}
+{%- assign already_recorded_today = false %}
+{%- for location in all_locations %}
+    {%- assign date = location.date | date: '%s' %}
+    {%- if date > today %}
+        {%- unless already_recorded_today %}
+            {%- assign previous_location_index = index | minus: 1 %}
+            {%- assign locations = locations | push: all_locations[previous_location_index] %}
+            {%- assign already_recorded_today = true %}
+        {%- endunless %}
+        {%- assign locations = locations | push: location %}
+    {%- endif %}
+    {%- assign index = index | plus: 1 %}
+{%- endfor -%}
 window.addEventListener('load', function () {
-    const locationsList = JSON.parse('{{ site.data.locations | jsonify }}');
+    const locationsList = JSON.parse('{{ locations | jsonify }}');
     const dayLength = 1000 * 60 * 60 * 24;
     const currentDate = new Date();
     const limitDate = new Date();
     limitDate.setDate(limitDate.getDate() + 14);
-    var currentLocation;
-    var nextLocation;
-    var nextLocationDate;
-    
-    var i = 0;
-    do {
-        let location = locationsList[i];
-        let locationDate = new Date(location.date);
-        if (currentDate < locationDate && locationDate <= limitDate) {
-            nextLocation = location;
-            nextLocationDate = locationDate;
-            break;
-        } else {
-            currentLocation = location;
-            i++;
-        }
-        if (i == locationsList.length) { break; }
-    } while (nextLocation == null);
 
+    const currentLocation = locationsList[0];
     document.querySelector('#current_location').textContent = currentLocation.label;
-
-    if (nextLocation != null) {
-        let days = Math.round(Math.abs(currentDate - nextLocationDate) / dayLength);
-        document.querySelector('#freccia').classList.remove('nascosto');
-        document.querySelector('#next_location').textContent = nextLocation.label;
-        let label = '';
-        if (days < 1) {
-            label = '(oggi)';
-        } else if (days === 1) {
-            label = '(tra 1 giorno)';
-        } else {
-            label = `(tra ${days} giorni)`;
+    
+    if (locationsList.length) {
+        const nextLocation = locationsList[1];
+        const nextLocationDate = new Date(nextLocation.date);
+        if (nextLocationDate <= limitDate) {
+            let days = Math.round(Math.abs(currentDate - nextLocationDate) / dayLength);
+            document.querySelector('#freccia').classList.remove('nascosto');
+            document.querySelector('#next_location').textContent = nextLocation.label;
+            let label = '';
+            if (days < 1) {
+                label = '(oggi)';
+            } else if (days === 1) {
+                label = '(tra 1 giorno)';
+            } else {
+                label = `(tra ${days} giorni)`;
+            }
+            document.querySelector('#when_next_location').textContent = label;
         }
-        document.querySelector('#when_next_location').textContent = label;
     }
 }, false);
