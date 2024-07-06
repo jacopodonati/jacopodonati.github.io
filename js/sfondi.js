c44606d368ee15610905ee9b861ae0883064a1f4
Jacopo Donati <33698919+jacopodonati@users.noreply.github.com>
2024-07-06 14:58:56 +0200

    Alleggerimento della landing page

---
+++ js/sfondi.js
@@ -0,0 +1,17 @@
+window.sfondi = [
+    {
+        filename: 'img001.webp',
+        background: '#21428f',
+        foreground: '#fcf5ea'
+    },
+    {
+        filename: 'img002.webp',
+        background: '#a88f85',
+        foreground: '#f6efe3'
+    },
+    {
+        filename: 'img009.webp',
+        background: '#000000',
+        foreground: '#ffffff'
+    }
+]
