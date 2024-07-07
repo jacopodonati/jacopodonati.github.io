7e2e77c5a0b598738a29145816643cef0ee3bc22
Jacopo Donati <33698919+jacopodonati@users.noreply.github.com>
2024-07-07 07:38:41 +0200

    Aggiunti i commenti identificativi degli sfondi

--- js/sfondi.js
+++ js/sfondi.js
@@ -1,16 +1,16 @@
 window.sfondi = [
     {
-        filename: 'img001.webp',
+        filename: 'img001.webp', // io da piccolo, strambo come sempre
         background: '#21428f',
         foreground: '#fcf5ea'
     },
     {
-        filename: 'img002.webp',
+        filename: 'img002.webp', // il mare in Sardegna
         background: '#a88f85',
         foreground: '#f6efe3'
     },
     {
-        filename: 'img009.webp',
+        filename: 'img009.webp', // foresta di Monte Cavallo
         background: '#000000',
         foreground: '#ffffff'
     }
