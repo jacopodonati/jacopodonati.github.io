d04c23eedaae4aa331c5e5f05d13315033d49446
Jacopo Donati <33698919+jacopodonati@users.noreply.github.com>
2024-07-07 07:38:57 +0200

    Aggiunta la collina di San Luca agli sfondi

--- js/sfondi.js
+++ js/sfondi.js
@@ -10,6 +10,11 @@ window.sfondi = [
         foreground: '#f6efe3'
     },
     {
+        filename: 'img003.webp', // la collina di San Luca innevata
+        background: '#f4f4ee',
+        foreground: '#40433a'
+    },
+    {
         filename: 'img009.webp', // foresta di Monte Cavallo
         background: '#000000',
         foreground: '#ffffff'
